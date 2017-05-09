8f38a8d0cb7be46e69be13cdd6fbb719abae5aa1
Hien Dang <hienpd@outlook.com>
2017-05-09 16:12:36 -0700

    Put user input into new list item

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 'use strict';
 
 let addForm = document.getElementById('add-form');
+let idCounter = 5;
 
-function addItem() {
+function addItem(e) {
+  let userInput = document.getElementById('add-item').value;
   let list = document.getElementById('list');
   let newItem = document.createElement('li');
-  let newText = document.createTextNode('test');
+  let newText = document.createTextNode(userInput);
   let newInput = document.createElement('input');
   let newLabel = document.createElement('label');
 
   newItem.className = 'collection-item';
   newInput.setAttribute('type', 'checkbox');
-  newLabel.setAttribute('for', 'test');
+  newLabel.setAttribute('for', userInput);
 
   newLabel.appendChild(newText);
   newItem.appendChild(newInput);
   newItem.appendChild(newLabel);
 
-  // list.appendChild(newItem);
   list.insertBefore(newItem, list.firstChild);
 }
 
-addForm.addEventListener('submit', function() {
-  addItem();
+addForm.addEventListener('submit', function(e) {
+  addItem(e);
 }, false);
