b0d956e3ae1e32dc43f5ea2c54b718f026370ce1
Hien Dang <hienpd@outlook.com>
2017-05-09 10:46:31 -0700

    Put in label and input tags

--- main.js
+++ main.js
@@ -2,15 +2,24 @@
 
 let addForm = document.getElementById('add-form');
 
-function addItem() {
+function addItem(e) {
   let list = document.getElementById('list');
   let newItem = document.createElement('li');
   let newText = document.createTextNode('test');
+  let newInput = document.createElement('input');
+  let newLabel = document.createElement('label');
 
   newItem.className = 'collection-item';
-  newItem.appendChild(newText);
+  newInput.setAttribute('type', 'checkbox');
+  newLabel.setAttribute('for', 'test');
+
+  newLabel.appendChild(newText);
+  newItem.appendChild(newInput);
+  newItem.appendChild(newLabel);
 
   list.appendChild(newItem);
 }
 
-addForm.addEventListener('submit', addItem, false);
+addForm.addEventListener('submit', function() {
+  addItem(e);
+}, false);
